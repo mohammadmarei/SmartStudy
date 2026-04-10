fef540e5d71f5ae71c25a96476f3ff9440e76b08
sondosalqutob <sondosalqutob@gmail.com>
2026-04-11 01:03:44 +0300

    migrations (user,profile,rule,refresh_token)

---
+++ database/migrations/2026_04_10_210410_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('role_id')
+                ->default(1)
+                ->constrained('roles')
+                ->onDelete('restrict');
+            $table->string('full_name');
+            $table->string('username')->unique()->nullable();
+            $table->string('email')->unique();
+            $table->string('password_hash');
+            $table->boolean('agreed_to_terms')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
